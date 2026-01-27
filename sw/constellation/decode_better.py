89c2407bb93becfa2b6ff265b355c28f439ae1c0
Elizaveta Sitnikova <elizaveta.sitnikova@desy.de>
2026-01-27 13:48:33 +0100

    SW: adding a configurable output decoding path

--- sw/constellation/decode_better.py
+++ sw/constellation/decode_better.py
@@ -2,10 +2,16 @@ from constellation.binary_decoder_better import Decoder
 import argparse
 import os
 
-def decode(filename, force, max_nreadouts, file_extensions, chip_version, fpga_ts_length, nchips_per_layer, nlayers):
+def decode(filename, force, max_nreadouts, file_extensions, chip_version, fpga_ts_length, nchips_per_layer, nlayers, outdir):
     os.makedirs('/'.join(filename.split('/')[:-1]), exist_ok=True)
 
-    name_output = filename.replace('raw_data', 'decoded')
+    if outdir is None:
+        name_output = filename.replace('raw_data', 'decoded')
+    else:
+        name_output = outdir
+        if not name_output.endswith('/'):
+            name_output += '/'
+        name_output += filename.split('/')[-1]
 
     if not force:
         all_files_exist = True
@@ -53,7 +59,7 @@ def main(args):
             file_extensions.append('.h5')
         if args.root:
             file_extensions.append('.root')
-        decode(filename, args.force, args.max_nreadouts, file_extensions, args.version, fpga_ts_length=args.fpga_ts_length, nchips_per_layer=args.nchips_per_layer, nlayers=args.nlayers)
+        decode(filename, args.force, args.max_nreadouts, file_extensions, args.version, fpga_ts_length=args.fpga_ts_length, nchips_per_layer=args.nchips_per_layer, nlayers=args.nlayers, outdir=args.outdir)
 
 
 if __name__ == "__main__":
@@ -70,6 +76,7 @@ if __name__ == "__main__":
     parser.add_argument('--fpga_ts_length', required=False, help='Length of the FPGA imestamp in bytes', type=int, default=None)
     parser.add_argument('--nchips_per_layer', required=False, help='Number of chips per layer', type=int, default=None)
     parser.add_argument('--nlayers', required=False, help='Number of layers', type=int, default=None)
+    parser.add_argument('-o', '--outdir', required=False, help='Directory for the output file', default=None)
 
     args = parser.parse_args()
     main(args)
