ae08b86ba301db921a970eacbf18451a2fa8c07b
Octopus User <octouser@octopus0.desy.de>
2026-04-10 05:19:29 -0400

    Changes in main for v4

--- sw/constellation-decode/__main__.py
+++ sw/constellation-decode/__main__.py
@@ -2,7 +2,8 @@ import argparse
 import os
 
 from .decoder_v3 import Decoder_v3
-from .common import DecoderSettings, Stats, MatcherStrategy
+from .decoder_v4 import Decoder_v4
+from .common import DecoderSettings_v3, DecoderSettings_v4, Stats_v3, Stats_v4, MatcherStrategy
 
 def decode(filename, force, file_extensions, chip_version, fpga_ts_length, nchips_per_layer, nlayers, outdir, max_readout_blocks, write_strip_files, matcher_strategy, use_tlu):
     os.makedirs('/'.join(filename.split('/')[:-1]), exist_ok=True)
@@ -24,15 +25,16 @@ def decode(filename, force, file_extensions, chip_version, fpga_ts_length, nchip
             print(f'File decoded with all ({file_extensions}) extensions, skipping')
             return
 
-    stats = Stats()
+    
 
     if chip_version == 3:
-        decoder_settings = DecoderSettings(
+        stats = Stats_v3()
+        decoder_settings = DecoderSettings_v3(
             nlayers=nlayers,
             nchips_per_layer=nchips_per_layer,
             fpga_ts_length=fpga_ts_length,
             fpga_ts_clock_freq=80e6,
-            fpga_ts_hh_filter_limit = 100000, # 100ks aka ~28h
+            fpga_ts_packet_filter_limit = None,#100000, # 100ks aka ~28h
             fpga_ts_matching_limit=3e-3, # 3ms
             matcher_strategy=matcher_strategy,
             matcher_ts_limit=2, # clk cycles
@@ -43,7 +45,17 @@ def decode(filename, force, file_extensions, chip_version, fpga_ts_length, nchip
         )
         d = Decoder_v3(filename, stats, decoder_settings)
     else:
-        raise ValueError(f"Decoder is not (yet) defined for chip version {chip_version}")
+        stats = Stats_v4()
+        decoder_settings = DecoderSettings_v4(
+            nlayers=nlayers,
+            nchips_per_layer=nchips_per_layer,
+            fpga_ts_length=fpga_ts_length,
+            fpga_ts_clock_freq=80e6,
+            fpga_ts_packet_filter_limit = None,#100000, # 100ks aka ~28h
+            max_readout_blocks=max_readout_blocks,
+            use_tlu=use_tlu
+        )
+        d = Decoder_v4(filename, stats, decoder_settings)
     if '.root' in file_extensions:
         d.prepare_root_file(name_output.replace('.bin', '.root'))
     if '.h5' in file_extensions:
